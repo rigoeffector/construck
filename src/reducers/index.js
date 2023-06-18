894b762ffb0abf0dbb0c3d698cd37ecb90c92b4d
rigoeffector <rigorene48@gmail.com>
2023-06-18 02:38:56 +0200

    Create Vendor Reducer and states mapping

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import {combineReducers} from 'redux';
 import loginUserReducer from './auth';
 import customizationReducer from '../store/customizationReducer';
 import snackbarReducer from '../store/snackbarReducer';
-import getVendorsListReducer  from './vendors'
+import getVendorsListReducer  from './vendors/read'
+import createVendorReducer from './vendors/create';
 const rootReducer = combineReducers({
     customization: customizationReducer,
     snackbar: snackbarReducer,
     auth: loginUserReducer,
-    listVendors: getVendorsListReducer
+    listVendors: getVendorsListReducer,
+    createVendor: createVendorReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/vendors/create.js
@@ -0,0 +1,38 @@
+import {
+    CREATE_VENDOR_ERROR,
+    CREATE_VENDOR_LOADING,
+    CREATE_VENDOR_SUCCESS,
+    CREATE_VENDOR_RESET,
+} from './constant';
+
+const initialState = {
+    loading: false,
+    success: false,
+    error: null,
+    data: null,
+    message: null
+};
+
+const createVendorReducer = (state = initialState, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case CREATE_VENDOR_LOADING:
+            return {...state, loading: payload.loading};
+        case CREATE_VENDOR_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                success: payload.success,
+                error: null,
+                data: payload.result
+            };
+        case CREATE_VENDOR_ERROR:
+            return {...state, loading: false, error: payload.error, message: payload.error_message};
+        case CREATE_VENDOR_RESET:
+            return initialState;
+        default:
+            return state;
+    }
+};
+
+export default createVendorReducer;
